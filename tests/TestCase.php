febc6785bd07e64bd6b6b16f5379f17aab994184
Mads Møller <mam@templafy.com>
2020-10-29 15:33:06 +0100

    github actions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -73,7 +73,12 @@ class TestCase extends \Orchestra\Testbench\TestCase
     protected function getEnvironmentSetUp($app)
     {
         $app['config']->set('cache.default', 'array');
-        $app['config']->set('database.default', 'test');
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
 
         $app['config']->set('acl', [
             'models' => [
@@ -92,7 +97,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
             'driver'      => 'mysql',
             'host'        => env('DB_HOST', '127.0.0.1'),
             'port'        => env('DB_PORT', '3306'),
-            'database'    => env('DB_DATABASE', 'db_testing'),
+            'database'    => env('DB_DATABASE', 'db_test'),
             'username'    => env('DB_USERNAME', 'root'),
             'password'    => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
